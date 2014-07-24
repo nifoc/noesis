d75615617386a360bec875c046cb0e8a4066aa5c
Daniel Kempkens <daniel.kempkens@gmail.com>
2014-07-24 22:10:08 +0200

    Improve typespec for internal levenshtein_rec

--- src/noesis_string.erl
+++ src/noesis_string.erl
@@ -44,7 +44,7 @@ hamming(First, Second) when length(First) =:= length(Second) ->
 
 % Private
 
--spec levenshtein_rec(string(), string(), noesis_dict:dictionary()) -> {non_neg_integer(), noesis_dict:dictionary()}.
+-spec levenshtein_rec(string(), string(), noesis_dict:dictionary(tuple(), non_neg_integer())) -> {non_neg_integer(), noesis_dict:dictionary(tuple(), non_neg_integer())}.
 levenshtein_rec(Str, Str, Dict) ->
   Dict2 = dict:store({Str, Str}, 0, Dict),
   {0, Dict2};
