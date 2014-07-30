acf0d7499e7a038e7421a4eef042a6bdd42ef65e
Daniel Kempkens <daniel.kempkens@gmail.com>
2014-07-30 13:40:14 +0200

    Improve pmap

--- src/noesis_lists.erl
+++ src/noesis_lists.erl
@@ -18,7 +18,8 @@
 % API
 -export([
   group_by/2,
-  pmap/2
+  pmap/2,
+  split/2
 ]).
 
 % API
@@ -34,29 +35,63 @@ group_by(Fun, List) ->
   dict:to_list(Dict).
 
 % @doc Takes a function from `A's to `B's and a list of `A's and produces a list of `B's by applying the function
-%      to every element in the list in parallel. This function is used to obtain the return values.<br /><br />
-%      Based on <a href="http://erlang.org/pipermail/erlang-questions/2006-June/020834.html" target="_blank">Erlang on the Niagara</a>
+%      to every element in the list in parallel. The function is used to obtain the return values.<br /><br />
+%      Partially based on <a href="http://erlang.org/pipermail/erlang-questions/2006-June/020834.html" target="_blank">Erlang on the Niagara</a>
 %      by Joe Armstrong.
 -spec pmap(fun((A) -> B), [A]) -> [B].
-pmap(_Fun, []) -> [];
+pmap(_Fun, []) ->
+  [];
 pmap(Fun, List) ->
-  Parent = self(),
-  Worker = lists:map(fun(Item) ->
-    spawn(fun() -> parallel_apply(Parent, Fun, Item) end)
-  end, List),
-  parallel_gather(Worker, []).
+  Ref = make_ref(),
+  Schedulers = erlang:system_info(schedulers),
+  {Chunks, Rest} = split(Schedulers, List),
+  Index = pmap_run(Fun, Ref, 1, Chunks),
+  pmap_maybe_run_and_gather(Fun, Ref, Rest, Index, length(List), 0, []).
+
+% @doc Splits `List' into `ListA' and `ListB'. `ListA' contains the first `N' elements and `ListB' the rest of the elements (the `N'th tail).
+%      If `N >= length(List)' this function will not throw an error (as opposed to `lists:split/2').<br /><br />
+%      <strong>Examples</strong><br />
+%      `split(2, [])' => `{[], []}'<br />
+%      `split(2, [demo])' => `{[demo], []}'<br />
+%      `split(2, [a, b, c, d, e])' => `{[a, b], [c, d, e]}'
+-spec split(non_neg_integer(), list()) -> {ListA :: list(), ListB :: list()}.
+split(_N, []) ->
+  {[], []};
+split(N, List) when N =< length(List) ->
+  lists:split(N, List);
+split(_N, List) ->
+  {List, []}.
 
 % Private
 
--spec parallel_apply(pid(), fun((A) -> B), A) -> B.
-parallel_apply(Parent, Fun, Item) ->
-  Worker = self(),
-  Value = (catch Fun(Item)),
-  Parent ! {Worker, Value}.
+-spec pmap_run(fun(), reference(), pos_integer(), list()) -> pos_integer().
+pmap_run(Fun, Ref, StartIndex, List) ->
+  Parent = self(),
+  lists:foldl(fun(Item, Index) ->
+    _Pid = spawn(fun() ->
+      Value = (catch Fun(Item)),
+      Parent ! {Index, Ref, Value}
+    end),
+    Index + 1
+  end, StartIndex, List).
 
--spec parallel_gather([pid()], list()) -> list().
-parallel_gather([], Acc) -> lists:reverse(Acc);
-parallel_gather([Pid|Rest], Acc) ->
+-spec pmap_maybe_run_and_gather(fun(), reference(), list(), pos_integer(), non_neg_integer(), non_neg_integer(), list()) -> list().
+pmap_maybe_run_and_gather(_Fun, _Ref, [], _Index, ResultLength, AccLength, Acc) when ResultLength =:= AccLength ->
+  Acc2 = lists:keysort(1, Acc),
+  {_Key, List} = lists:unzip(Acc2),
+  List;
+pmap_maybe_run_and_gather(Fun, Ref, [], Index, ResultLength, AccLength, Acc) ->
   receive
-    {Pid, Value} -> parallel_gather(Rest, [Value | Acc])
+    {ResultIndex, Ref, Value} ->
+      AccLength2 = AccLength + 1,
+      Acc2 = [{ResultIndex, Value} | Acc],
+      pmap_maybe_run_and_gather(Fun, Ref, [], Index, ResultLength, AccLength2, Acc2)
+  end;
+pmap_maybe_run_and_gather(Fun, Ref, [Chunk|Rest], Index, ResultLength, AccLength, Acc) ->
+  receive
+    {ResultIndex, Ref, Value} ->
+      Index2 = pmap_run(Fun, Ref, Index, [Chunk]),
+      AccLength2 = AccLength + 1,
+      Acc2 = [{ResultIndex, Value} | Acc],
+      pmap_maybe_run_and_gather(Fun, Ref, Rest, Index2, ResultLength, AccLength2, Acc2)
   end.
