78e4511e8859546e328def6e9c1488f464891103
Daniel Kempkens <daniel+git@kempkens.io>
2015-03-19 21:53:10 +0100

    More polyline tests

--- test/noesis_polyline_test.erl
+++ test/noesis_polyline_test.erl
@@ -14,8 +14,10 @@
 
 encode_test() ->
   ?assertEqual(<<"_p~iF~ps|U_ulLnnqC_mqNvxq`@">>, noesis_polyline:encode([{-120.2, 38.5}, {-120.95, 40.7}, {-126.453, 43.252}])),
-  ?assertEqual(<<"}wjiGtdpcNrAlBJZ">>, noesis_polyline:encode([{-79.38651, 43.64175}, {-79.38706, 43.64133}, {-79.3872, 43.64127}])).
+  ?assertEqual(<<"}wjiGtdpcNrAlBJZ">>, noesis_polyline:encode([{-79.38651, 43.64175}, {-79.38706, 43.64133}, {-79.3872, 43.64127}])),
+  ?assertEqual(<<"_ojiHa`tLh{IdCw{Gwc_@">>, noesis_polyline:encode([{2.23761, 48.8832}, {2.23694, 48.82747}, {2.40154, 48.87303}])).
 
 decode_test() ->
   ?assertEqual([{-120.2, 38.5}, {-120.95, 40.7}, {-126.453, 43.252}], noesis_polyline:decode(<<"_p~iF~ps|U_ulLnnqC_mqNvxq`@">>)),
-  ?assertEqual([{-79.38651, 43.64175}, {-79.38706, 43.64133}, {-79.3872, 43.64127}], noesis_polyline:decode(<<"}wjiGtdpcNrAlBJZ">>)).
+  ?assertEqual([{-79.38651, 43.64175}, {-79.38706, 43.64133}, {-79.3872, 43.64127}], noesis_polyline:decode(<<"}wjiGtdpcNrAlBJZ">>)),
+  ?assertEqual([{2.23761, 48.8832}, {2.23694, 48.82747}, {2.40154, 48.87303}], noesis_polyline:decode(<<"_ojiHa`tLh{IdCw{Gwc_@">>)).
