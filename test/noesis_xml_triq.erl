dddeedeb262b270e4c32887267bdc3233a044871
Daniel Kempkens <daniel+git@kempkens.io>
2015-03-28 21:08:28 +0100

    Expand noesis_xml triq suite

--- test/noesis_xml_triq.erl
+++ test/noesis_xml_triq.erl
@@ -20,13 +20,21 @@ string() -> [char()].
 
 float() -> ?LET({A, B}, {int(), pos_integer()}, A / B * 100).
 
-list_key() -> oneof([binary(), atom(), string()]).
+non_empty_binary() -> ?SUCHTHAT(B, binary(), byte_size(B) > 0).
+
+non_empty_atom() -> ?SUCHTHAT(A, atom(), byte_size(atom_to_binary(A, utf8)) > 0).
+
+non_empty_string() -> ?SUCHTHAT(S, string(), length(S) > 0).
+
+list_key() -> oneof([non_empty_binary(), non_empty_atom(), non_empty_string()]).
 
 list_value_simple() -> oneof([binary(), string(), int(), float()]).
 
 list_value() -> oneof([list_value_simple(), [{list_key(), list_value_simple()}]]).
 
 proplist_simple() -> list({list_key(), list_value()}).
+
+proplist_simple_string() -> list({non_empty_string(), non_empty_string()}).
 
 proplist_attributes() -> list({list_key(), [{list_key(), list_value_simple()}], list_value()}).
 
@@ -37,3 +45,12 @@ proplist() -> oneof([proplist_simple(), proplist_attributes()]).
 prop_from_list_1() ->
   numtests(200, ?FORALL(List, proplist(),
     is_binary(noesis_xml:from_list(List)))).
+
+prop_from_list_2() ->
+  ?FORALL(List, proplist_simple_string(),
+    ?IMPLIES(length(List) > 0,
+      begin
+        Xml = unicode:characters_to_list(noesis_xml:from_list(List)),
+        {Doc, _Rest} = xmerl_scan:string(Xml),
+        is_tuple(Doc)
+      end)).
